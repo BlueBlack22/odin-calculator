13528b7731bbbdfb0afa80ca7d5252686964b09f
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-22 22:23:54 +0200

    Added working display.
    Number buttons now display corresponding numbers on the display.

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-let displayValue = 0;
+let displayValue = '0';
 
-function add(firstNum, secondNum) {
-    return firstNum + secondNum;
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+
+function add(x, y) {
+    return x + y;
 }
 
-function subtract(firstNum, secondNum) {
-    return firstNum - secondNum;
+function subtract(x, y) {
+    return x - y;
 }
 
-function multiply(firstNum, secondNum) {
-    return firstNum * secondNum;
+function multiply(x, y) {
+    return x * y;
 }
 
-function divide(firstNum, secondNum) {
-    return firstNum / secondNum;
+function divide(x, y) {
+    if (y == 0) {
+        return 'no';
+    } else {
+        return x / y;
+    }
 }
 
-function updateMainDisplay() {
+function updateMainDisplay(value) {
+    const mainDisplay = document.querySelector('#display-main');
+    
+    if (displayValue.length >= 8) {
 
+    }
+    else if (displayValue == '0' && value != 'decimal') {
+        mainDisplay.textContent = value;
+    } else {
+        mainDisplay.textContent = displayValue + value;
+    }
+    displayValue = mainDisplay.textContent;
 }
 
 function updateSecondaryDisplay() {
@@ -25,7 +43,30 @@ function updateSecondaryDisplay() {
 }
 
 function operate(operator, firstNum, secondNum) {
-    
+    switch (operator) {
+        case 'add': {
+            console.log(add(firstNum, secondNum));
+            break;
+        }
+
+        case 'subtract': {
+            subtract(firstNum, secondNum);
+            break;
+        }
+
+        case 'multiply': {
+            multiply(firstNum, secondNum);
+            break;
+        }
+
+        case 'divide': {
+            divide(firstNum, secondNum);
+            break;
+        }
+    }
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.number-button');
+buttons.forEach(function(currentBtn) {
+    currentBtn.addEventListener('click', (e) => updateMainDisplay(e.target.value));
+});
