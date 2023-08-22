d0d87b0c1921e6426dc4d17d06b99b551ee44b51
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-22 18:44:27 +0200

    Added basic JS layout.

---
+++ script.js
@@ -0,0 +1,31 @@
+let displayValue = 0;
+
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+function updateMainDisplay() {
+
+}
+
+function updateSecondaryDisplay() {
+
+}
+
+function operate(operator, firstNum, secondNum) {
+    
+}
+
+const buttons = document.querySelectorAll('button');
